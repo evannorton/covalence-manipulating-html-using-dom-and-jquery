706282e40c16fd17b87ee7b0a1725dff01a27269
Evan Norton <evanan39@yahoo.com>
2018-01-12 10:59:13 -0600

    DOM: create button that logs nice message

---
+++ DOM/scripts.js
@@ -0,0 +1,12 @@
+addEventListener("DOMContentLoaded", button);
+function button() {
+    let button = document.createElement("button");
+    let buttonText = document.createTextNode("Click Me!");
+    button.appendChild(buttonText);
+    document.body.appendChild(button);
+
+    addEventListener("click", click);
+    function click() {
+        console.log("This is a very nice message.")
+    }
+}
