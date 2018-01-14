757bd4e309fba6db245c9159a329621f28ad9eaf
Evan Norton <evanan39@yahoo.com>
2018-01-13 19:01:06 -0500

    objective 2

--- JQuery/scripts.js
+++ JQuery/scripts.js
@@ -1,5 +1,13 @@
+/* OBJECTIVE 1
 let $button = $("<button>Click Me!</button>");
 $button.appendTo("body");
 $button.click(function () {
     alert("Hey nice person.")
+});
+*/
+
+let $button = $("#button");
+$button.click( function () {
+    let message = $("#text-box").val();
+    alert(message);
 });
